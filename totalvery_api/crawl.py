899c9ecaa57c7910f57eed6a47da9e0387c590b2
symoon94 <msy0128@gmail.com>
2021-01-04 21:59:54 +0900

    update package

--- totalvery_api/crawl.py
+++ totalvery_api/crawl.py
@@ -6,18 +6,31 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+
+from subprocess import check_output
+import time
+
+from totalvery_api.delivery_services.crawler import UbereatsCrawler, DoordashCrawler, GrubhubCrawler
+
+
 @csrf_exempt
 @api_view(['POST'])
 def store_detail(request):
     if request.method == 'POST':
         serializer = RestaurantIDsSerializer(data=request.data)
         if serializer.is_valid():
+            # crawler = UbereatsCrawler()
+            # crawler = DoordashCrawler()
+            crawler = GrubhubCrawler()
+
+            store_json = crawler.get_store(serializer.data["grubhubID"])
 
             # TODO: ubereats, doordash, grubhub에 각 id로 레스토랑 디테일 호출해와서 파싱한 뒤 적절한 것만 추려서 json으로 return
 
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(store_json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
