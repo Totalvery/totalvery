44ca4d4e119b33b9593e81b3621b7e4a7502bbcd
symoon94 <msy0128@gmail.com>
2021-01-11 21:16:14 +0900

    create dummy and fix now showing ubereats fee

--- totalvery_api/tests.py
+++ totalvery_api/tests.py
@@ -10,15 +10,19 @@ headers = {
 
 # TODO: cart size도 넘겨주기
 payload = json.dumps({"meta": {"ubereats": True, "doordash": True, "grubhub": True}, "ids": {"ubereatsID": "65f472f1-5f54-4429-8956-0774ffee6cdd", "doordashID": "540546", "grubhubID": "332063"}, "customer_location": {"latitude": 32.2250168,
-                                                                                                           "longitude": -110.9539833}})
+                                                                                                                                                                                                                         "longitude": -110.9539833}})
 
-import ipdb; ipdb.set_trace()
+ipdb.set_trace()
 res = requests.post("http://127.0.0.1:8000/api/getStoreDetails/",
                     headers=headers, data=payload)
 
 ipdb.set_trace()
 
 dic = res.json()
+
+with open('totalvery_api/data/getStoreDetails.json', mode='w') as f:
+    json.dump(dic, f)
+
 
 
 # from totalvery_api.delivery_services.crawler import UbereatsCrawler, DoordashCrawler, GrubhubCrawler
