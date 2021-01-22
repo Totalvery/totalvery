a57f5285a781d13be444c199f5105ca76d053ef3
SarahHyunjuSong <hyunjusong@email.arizona.edu>
2021-01-23 08:45:08 +0900

    remove django_heroku

--- totalvery/settings.py
+++ totalvery/settings.py
@@ -12,12 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# Configure app for Heroku deployment
-django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
