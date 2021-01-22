a923677559dfa790ce64b32be6b62a6736b3f9a9
SarahHyunjuSong <hyunjusong@email.arizona.edu>
2021-01-23 08:11:32 +0900

    add django-heroku

--- totalvery/settings.py
+++ totalvery/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# Configure app for Heroku deployment
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
