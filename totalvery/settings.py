7ee28c58c0f6b087e979bd1fb7d5a2d485db92cb
SarahHyunjuSong <hyunjusong@email.arizona.edu>
2021-01-24 00:09:06 +0900

    add cors whitelist with other updates

--- totalvery/settings.py
+++ totalvery/settings.py
@@ -62,7 +62,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
+    'http://localhost:3000',
+    'http://localhost:8000',
 )
 
 ROOT_URLCONF = "totalvery.urls"
