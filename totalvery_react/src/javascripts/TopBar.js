9e990329de083cb7ca4f8bdbf6ff21e25139635e
Hyoseso Kwag <cloudymonkey@naver.com>
2021-01-31 20:55:29 -0700

    feed completion

--- totalvery_react/src/javascripts/TopBar.js
+++ totalvery_react/src/javascripts/TopBar.js
@@ -4,30 +4,31 @@ import marker_icon from "../images/marker_white.png";
 import menu_icon_24 from "../images/menu_icon_24.png";
 import menu_icon_48 from "../images/menu_icon_48.png";
 
-function TopBar() {
-    return(
-        <div>
-            <Navbar fixed="top" expand="lg" className="topbar">
-                <img
-                    src={marker_icon}
-                    width="20px"
-                    height="30px"
-                    alt="Location Marker"
-                />
-
-                <Navbar.Brand className="totalvery_title" href="/" style={{color:"white", textAlign:"center", fontFamily:'Philosopher',  fontSize: "30px"}}>TOTALVERY</Navbar.Brand>
-                {/* <Navbar.Toggle aria-controls="bar_menu" />
-                <Navbar.Collapse id="bar_menu">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/" style={{color:"white"}}>Home</Nav.Link>
-                        <Nav.Link href="/login" style={{color:"white"}}>Login</Nav.Link>
-                        <Nav.Link href="/cart" style={{color:"white"}}>Cart</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse> */}
-            </Navbar>
-        </div>
-        
-    )
+class TopBar extends React.Component {
+    render() {
+        return(
+            <div>
+                <Navbar fixed="top" expand="lg" className="topbar">
+                    <img
+                        src={marker_icon}
+                        width="20px"
+                        height="30px"
+                        alt="Location Marker"
+                    />
+                    <t className="topbar_location" style={{color:"white", fontFamily:'Philosopher'}}>{this.props.location}</t>
+                    <Navbar.Brand className="totalvery_title" href="/" style={{color:"white", textAlign:"center", fontFamily:'Philosopher',  fontSize: "30px"}}>TOTALVERY</Navbar.Brand>
+                    {/* <Navbar.Toggle aria-controls="bar_menu" /> */}
+                    {/* <Navbar.Collapse id="bar_menu">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/" style={{color:"white"}}>Home</Nav.Link>
+                            <Nav.Link href="/login" style={{color:"white"}}>Login</Nav.Link>
+                            <Nav.Link href="/cart" style={{color:"white"}}>Cart</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse> */}
+                </Navbar>
+            </div>
+        )
+    }
 }
 
 export default TopBar;
