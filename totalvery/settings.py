f4a3899bbee1d7a19b85763e22aa02badbe9ace5
whoareyou <whoareyou@gmail.com>
2022-11-15 23:10:45 +0900

    delete key

--- totalvery/settings.py
+++ totalvery/settings.py
@@ -21,10 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '-r8qu!atb9it1lf+5t&9*^9u1fivj^f-!lrnz=^+v4@!(vr+)^'
-
-# # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # ALLOWED_HOSTS = []
